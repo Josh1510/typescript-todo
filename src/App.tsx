41a007910bf423614837020d23dd2ace61aa1e1b
Joshua Moon <joshmoon.1510@gmail.com>
2022-01-20 17:13:52 +1100

    basic format of site

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <header>To Do List</header>
+      <body>
+        <div>To Do</div>
+        <div>Completed</div>
+      </body>
+      <footer>
+        <div>
+          <a href="https://github.com/Josh1510/typescript-todo">
+            github project
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
